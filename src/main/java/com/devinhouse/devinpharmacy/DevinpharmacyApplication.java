449e2d8170d4b425fc6b11f4b39fc0b8032b2011
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-11-28 14:43:21 -0800

    feat-estrutura-inicial

---
+++ src/main/java/com/devinhouse/devinpharmacy/DevinpharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse.devinpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinpharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinpharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devinhouse/devinpharmacy/DevinpharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.devinpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinpharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
