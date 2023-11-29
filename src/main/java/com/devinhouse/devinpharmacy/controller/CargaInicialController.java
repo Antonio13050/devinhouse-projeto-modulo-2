fd4b9e1be678108c63af4c28e0fb135e17901044
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-11-29 14:43:18 -0800

    feat-carga-inicial-add-mais-uma-farmacia

--- src/main/java/com/devinhouse/devinpharmacy/controller/CargaInicialController.java
+++ src/main/java/com/devinhouse/devinpharmacy/controller/CargaInicialController.java
@@ -29,6 +29,7 @@ public class CargaInicialController {
     public ResponseEntity<?> cargaInicial(){
         var medicamentos = medicamenteService.listAll();
         if (medicamentos.isEmpty()){
+
             MedicamentoRequestDTO medRequest1 = new MedicamentoRequestDTO(1010, "Programapan", "Matrix", "2x ao dia", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eleifend",111.00F, TipoMedicamento.COMUM);
             MedicamentoRequestDTO medRequest2 = new MedicamentoRequestDTO(7473, "Cafex", "Colombia Farm", "4x ao dia", "Pellentesque non ultricies mauris, ut lobortis elit. Cras nec cursus nibh",51.50F, TipoMedicamento.COMUM);
             MedicamentoRequestDTO medRequest3 = new MedicamentoRequestDTO(2233, "Estomazol", "Acme", "1x ao dia", "Sed risus mauris, consectetur eget egestas vitae",22.50F, TipoMedicamento.COMUM);
@@ -44,13 +45,17 @@ public class CargaInicialController {
         }
 
         var farmacias = farmaciaService.listAll();
-        if(farmacias.isEmpty()){
+       // if(farmacias.isEmpty()){
 
             EnderecoRequestDTO enderecoRequest1 = new EnderecoRequestDTO(88888999L, "Rua Porto Real", 67, "Westeros", "Berlim", "SC", "", 15.23456,  2.8678687);
             FarmaciaRequestDTO farRequest1 = new FarmaciaRequestDTO(90561736000121L, "DevMed Ltda", "Farmácia DevMed", "devmed@farmacia.com", "(44)4444-4444", "(44)9444-4441", enderecoRequest1);
 
+            EnderecoRequestDTO enderecoRequest2 = new EnderecoRequestDTO( 8877799L, "Rua Madrid", 76, "Winterfell", "Estocolmo", "SC", "", 19.254356, 3.8987687);
+            FarmaciaRequestDTO farRequest2 = new FarmaciaRequestDTO(43178995000198L, "MedHouse Ltda", "Farmácia MedHouse", "medhouse@farmacia.com", "(55)5555-5555", "(45)95555-5555", enderecoRequest2);
+
             farmaciaService.create(farRequest1);
-        }
+            farmaciaService.create(farRequest2);
+        //}
         return ResponseEntity.ok().build();
     }
 }
