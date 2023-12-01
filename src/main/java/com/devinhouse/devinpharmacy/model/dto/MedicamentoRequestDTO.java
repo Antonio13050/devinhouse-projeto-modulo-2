f22b6079323b7e3a9493bb22893571db4ef609b5
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-12-01 15:05:13 -0800

    feat-rf06-rn01-validacao-dos-campos

--- src/main/java/com/devinhouse/devinpharmacy/model/dto/MedicamentoRequestDTO.java
+++ src/main/java/com/devinhouse/devinpharmacy/model/dto/MedicamentoRequestDTO.java
@@ -1,8 +1,8 @@
 package com.devinhouse.devinpharmacy.model.dto;
 
 import com.devinhouse.devinpharmacy.model.enums.TipoMedicamento;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record MedicamentoRequestDTO(Integer nroRegistro, String nome, String laboratorio, String dosagem, String descricao, Float preco, TipoMedicamento tipo) {
+public record MedicamentoRequestDTO(@NotNull Integer nroRegistro, @NotBlank String nome, @NotBlank String laboratorio, @NotBlank String dosagem, String descricao, @NotNull Float preco, @NotNull TipoMedicamento tipo) {
 }
