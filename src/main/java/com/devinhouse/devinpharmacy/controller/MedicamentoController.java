c4585707c1301393d81ac31049933dc5d63e3e45
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-12-01 14:56:12 -0800

    feat-criado-endpoint-cadastro-de-medicamento

--- src/main/java/com/devinhouse/devinpharmacy/controller/MedicamentoController.java
+++ src/main/java/com/devinhouse/devinpharmacy/controller/MedicamentoController.java
@@ -1,13 +1,15 @@
 package com.devinhouse.devinpharmacy.controller;
 
+import com.devinhouse.devinpharmacy.model.dto.FarmaciaRequestDTO;
 import com.devinhouse.devinpharmacy.model.dto.FarmaciaResponseDTO;
+import com.devinhouse.devinpharmacy.model.dto.MedicamentoRequestDTO;
 import com.devinhouse.devinpharmacy.model.dto.MedicamentoResponseDTO;
 import com.devinhouse.devinpharmacy.service.MedicamenteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +24,11 @@ public class MedicamentoController {
     public ResponseEntity<List<MedicamentoResponseDTO>> listAll(){
         List<MedicamentoResponseDTO> response = this.medicamenteService.listAll();
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<MedicamentoResponseDTO> create(@RequestBody @Valid MedicamentoRequestDTO body){
+        MedicamentoResponseDTO response = this.medicamenteService.create(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
