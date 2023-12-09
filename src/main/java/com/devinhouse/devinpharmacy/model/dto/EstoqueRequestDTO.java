5478664f2cf12c1504abbac1db3f7838b39c5957
Antonio Carlos Virissimo <antonio13050@gmail.com>
2023-12-09 15:50:48 -0800

    feat-rf09-rn03-valida-quantidade-numero-positivo

--- src/main/java/com/devinhouse/devinpharmacy/model/dto/EstoqueRequestDTO.java
+++ src/main/java/com/devinhouse/devinpharmacy/model/dto/EstoqueRequestDTO.java
@@ -3,5 +3,5 @@ package com.devinhouse.devinpharmacy.model.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record EstoqueRequestDTO(@NotNull Long cnpj, @NotNull Integer nroRegistro, @Positive(message = "A quantidade deve ser um número positivo.") @NotNull Integer quantidade) {
+public record EstoqueRequestDTO(@NotNull Long cnpj, @NotNull Integer nroRegistro , @Positive(message = "A quantidade deve ser um número positivo.") @NotNull Integer quantidade) {
 }
